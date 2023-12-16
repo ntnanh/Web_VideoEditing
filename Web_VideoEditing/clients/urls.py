d60cfa4c3725f9c83f072fedec9e62f53aec4d88
Thien Viva <ngocanh2562001@gmail.com>
2023-12-16 21:53:03 +0700

    add

--- Web_VideoEditing/clients/urls.py
+++ Web_VideoEditing/clients/urls.py
@@ -4,5 +4,13 @@ from .import views
 urlpatterns = [
     path('', views.clients)
     # path('2/', views.clients2)
-
 ]
+
+
+
+
+# urlpatterns = [
+#     path('index/', views.clients, name='index'),
+#     path('all_tools/', views.clients, name='all_tools'),
+
+# ]
--- Web_VideoEditing/clients/views.py
+++ Web_VideoEditing/clients/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 def clients(request):
     return render(request, 'clients/index.html')
+
+
+
 # def clients2(request):
 #     return HttpResponse('hello world 111')
+# def all_tools(request):
+#     return render(request, 'clients/all_tools.html')
+# def index(request):
+#     return render(request, 'clients/index.html')
