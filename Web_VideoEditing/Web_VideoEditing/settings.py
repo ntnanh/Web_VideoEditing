fc5eb1606fd41aa1e7e284757906c499b8d6e133
thuynga1112 <thuyngapdl12b3@gmail.com>
2023-12-18 16:03:37 +0700

    user_project

--- Web_VideoEditing/Web_VideoEditing/settings.py
+++ Web_VideoEditing/Web_VideoEditing/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT= os.path.join(BASE_DIR, 'static'),
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- Web_VideoEditing/administrators/apps.py
+++ Web_VideoEditing/administrators/apps.py
@@ -2,10 +2,5 @@ from django.apps import AppConfig
 
 
 class AdministratorsConfig(AppConfig):
-<<<<<<< HEAD
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'administrators'
-=======
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "administrators"
->>>>>>> 973a9d8e5c43f98420d713d97950160a6318f38c
