4ad4d6885da7d44d35056e94eba492df04886ec3
thuynga1112 <thuyngapdl12b3@gmail.com>
2023-12-19 15:25:47 +0700

    up2

--- Web_VideoEditing/Web_VideoEditing/urls.py
+++ Web_VideoEditing/Web_VideoEditing/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('users.urls')),
     path('clients/', include('clients.urls')),
-    path('', include('user_project.urls')),
+    path('', include('user_project.urls', namespace='user_project')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Web_VideoEditing/users/models.py
+++ Web_VideoEditing/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from PIL import Image
+
 
 # Create your models here.
 class Users(models.Model):
@@ -8,12 +10,21 @@ class Users(models.Model):
     password = models.CharField(max_length=20)
     create_at = models.DateTimeField(default=timezone.now)
     role = models.CharField(max_length=50, default='client')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     
     class Meta:
         db_table = "users"
         
     def __str__(self):
         return self.username
-    
-    empAuth_objects = models.Manager()
+
+    def save(self, *args, **kwargs):
+        super(Users, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
     
