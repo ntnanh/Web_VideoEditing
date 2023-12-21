635234acc637ab667b4c736e4e7b92f81d62596a
Thien Viva <ngocanh2562001@gmail.com>
2023-12-21 14:55:24 +0700

    add1

--- Web_VideoEditing/clients/views.py
+++ Web_VideoEditing/clients/views.py
@@ -11,7 +11,7 @@ def all_tools(request):
 def add_subtitles(request):
     return render(request, 'clients/add_subtitles.html')
 
-<<<<<<< HEAD
+
 def merge_video(request):
     return render(request, 'clients/merge_video.html')
 
@@ -34,7 +34,6 @@ def loop_video(request):
 
 def loop_tool(request):
     return render(request, 'clients/loop_tool.html')
->>>>>>> cde8e9d14b0e011e4f044797764594c817295970
 
 # def clients(request):
 #     return render(request, 'clients/index.html')
