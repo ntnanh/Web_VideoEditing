3996467466d4f132956db5447b513cc9c24e6971
Thien Viva <ngocanh2562001@gmail.com>
2023-12-28 16:21:02 +0700

    admin

--- Web_VideoEditing/administrators/urls.py
+++ Web_VideoEditing/administrators/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.index_admin, name="index_admin"),
+    path('', views.index, name="index"),
     path('detail_user', views.detail_user, name="detail_user"),
-    
+    # path('index', views.index, name="index"),
+    path('table', views.table, name="table"),
+
+
 ]
--- Web_VideoEditing/administrators/views.py
+++ Web_VideoEditing/administrators/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index_admin(request):
-    return render(request, 'administrators/index_admin.html')
+# def index_admin(request):
+#     return render(request, 'administrators/index_admin.html')
 
 def detail_user(request):
     return render(request, 'administrators/detail_user.html')
+
+def index(request):
+    return render(request, 'administrators/index.html')
+
+def table(request):
+    return render(request, 'administrators/table.html')
