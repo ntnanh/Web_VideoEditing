0e9dc6a73ea815c82e06cf842ecb5d191c29e494
Thien Viva <ngocanh2562001@gmail.com>
2023-12-29 10:06:09 +0700

    user_project

--- Web_VideoEditing/user_project/views.py
+++ Web_VideoEditing/user_project/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from users.models import Users
 from django.http import HttpResponse
+from django.urls import reverse
+
 # Create your views here.
 def base_context(request):
     username = request.session.get('username', None)
@@ -10,3 +12,6 @@ def base_context(request):
 def home(request):
     context = base_context(request)
     return render(request, 'user_project/index.html', context)
+
+def forgot_password(request):
+    return render(request, 'user_project/forgot_password.html')
