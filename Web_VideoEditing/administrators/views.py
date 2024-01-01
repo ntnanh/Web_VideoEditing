836c62550ff7e61041e35d959821e491214ef3ca
thuynga1112 <thuyngapdl12b3@gmail.com>
2024-01-01 19:59:32 +0700

    up11

--- Web_VideoEditing/administrators/views.py
+++ Web_VideoEditing/administrators/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from users.views import base_context
 
 # Create your views here.
 # def index_admin(request):
 #     return render(request, 'administrators/index_admin.html')
 
 def detail_user(request):
-    return render(request, 'administrators/detail_user.html')
+    context = base_context(request)
+    return render(request, 'administrators/detail_user.html',context)
 
 def index(request):
-    return render(request, 'administrators/index.html')
+    context = base_context(request)
+    return render(request, 'administrators/index.html',context)
 
 def table(request):
-    return render(request, 'administrators/table.html')
+    context = base_context(request)
+    return render(request, 'administrators/table.html',context)
--- Web_VideoEditing/users/views.py
+++ Web_VideoEditing/users/views.py
@@ -1,23 +1,52 @@
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
-from django.http import HttpResponse, HttpResponseBadRequest
-from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.contrib import messages
 from .models import Users
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.contrib.auth.hashers import make_password
 
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
-        mysqluser = Users(username=username, email=email, password=password)
+
+        # check username
+        if Users.objects.filter(username=username).exists():
+            error_message = '**Username already exists.'
+            return JsonResponse({'error_message': error_message})
+        
+        # Kiểm tra email và tính hợp lệ của email
+        try:
+            Users.objects.get(email=email)
+            email_message = '**Email already exists.'
+            return JsonResponse({'email_message': email_message})
+        except Users.DoesNotExist:
+            try:
+                validate_email(email)
+            except ValidationError:
+                email_message = '**Invalid email.'
+                return JsonResponse({'email_message': email_message})
+        
+        #check password least 8 characters
+        if len(password) < 8:
+            password_message = '**Password must be at least 8 characters long.'
+            return JsonResponse({'password_message': password_message})
+        
+        hashed_password = make_password(password)
+        mysqluser = Users(username=username, email=email, password=hashed_password)
         mysqluser.save()
+        
         return redirect('users:signin')
-    
-    
+
     return render(request, 'users/signup.html')
 
 def signin(request):
+    error_message = None  # Biến lưu trữ thông báo lỗi
+
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
@@ -26,18 +55,32 @@ def signin(request):
             user = Users.objects.get(email=email, password=password)
             if user is not None:
                 request.session['username'] = user.username
-                return redirect('clients:home')
+                if user.role == 'admin':
+                    return redirect('administrators:index')
+                elif user.role == 'client':
+                    return redirect('clients:home')
+                
             else:
-                print("Some tried to login and failed")
+                error_message = "Email or password is incorrect."  # Gán thông báo lỗi
+                
+                print("Someone tried to login and failed")
                 print("They used email: {} and password: {}".format(email, password))
                 
-                return redirect('/')
+                return render(request, 'users/signin.html', {'error_message': error_message})
         except Users.DoesNotExist:
+            error_message = "User does not exist."  # Gán thông báo lỗi
+            
             print("User does not exist")
-            return redirect("/")
+            
+            return render(request, 'users/signin.html', {'error_message': error_message})
     
-    return render(request, 'users/signin.html')
+    return render(request, 'users/signin.html', {'error_message': error_message})
     
+def base_context(request):
+    username = request.session.get('username', None)
+    user_profile = Users.objects.get(username=username) if username else None
+    return {'user_profile': user_profile}
+
 def signout(request):
     logout(request)
     return redirect('clients:home')
