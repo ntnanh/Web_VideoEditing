e514f447d3ee0339f5fcbc1981b66489ba79cffd
Quan Nguyen <quan.nguyen@tomodev.vn>
2024-01-06 21:02:08 +0700

    update alert select file

--- Web_VideoEditing/clients/static/clients/js/upload.js
+++ Web_VideoEditing/clients/static/clients/js/upload.js
@@ -2,7 +2,20 @@ function Upload(){
 
     const handleUploadCutVideo = function () {
         $('#upload-file').on('change', function () {
-            $('#formUpload').submit();
+            const fileInput = $('#upload-file');
+            const selectedFile = fileInput[0].files[0];
+
+            if (selectedFile) {
+                if (selectedFile.type === 'video/mp4') {
+                    $('#formUpload').submit();
+                } else {
+                    // Notify the user that the selected file is not an .mp4 file
+                    alert('Please select a .mp4 file.');
+                }
+            } else {
+                // Notify the user to select a file
+                alert('Please select a file.');
+            }
         })
     }
 
